b5fb14ed3f46157314e204cae6cbfc43431ac82b
Yana Hvozdetska <yhvozdetska@gmail.com>
2022-05-19 10:42:33 +0200

    deleted all code
    Signed-off-by: Yana Hvozdetska <yhvozdetska@gmail.com>

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -1,24 +1,12 @@
 package com.example.springbootdemo;
 
-import lombok.AllArgsConstructor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-@AllArgsConstructor
+
 @SpringBootApplication
-public class SpringbootdemoApplication{
- private final BigBike bigBike;
-private final BigBikeWithListInjected bigBikeWithListInjected;
+public class SpringbootdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
-	}
-	@EventListener(ContextRefreshedEvent.class)
-	public void bigBikeMethod() {
-		bigBike.allDependents();
-	}
-	@EventListener(ContextRefreshedEvent.class)
-	public void bigBikeWithListInject() {
-		bigBikeWithListInjected.elements();
 	}
 }
